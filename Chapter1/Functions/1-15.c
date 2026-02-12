0674470e76fdd6124577b79a0bf06eb5777e9558
Neuromancer <m.arda.safak06@gmail.com>
2026-02-12 18:51:59 +0300

    First exercise for Functions.

---
+++ Chapter1/Functions/1-15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//A program that converts Celc to Fahr or vise versa
+
+
+float celcToFahr(float celc);
+float fahrToCelc(float fahr);
+
+
+int main()
+{
+
+	return(0);
+}
+
+
+float celcToFahr(float celc)
+{
+	return((9.0*celc/5.0)+32.0); 
+}
+
+float fahrToCelc(float fahr)
+{
+	return(5*0(fahr-32.0)/9.0);
+}
+
