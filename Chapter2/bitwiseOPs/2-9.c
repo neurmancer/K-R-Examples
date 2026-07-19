93021e5171bd6707cfab7e8cc62a89d1696a235d
Neuromancer <m.arda.safak06@gmail.com>
2026-07-19 17:56:30 +0300

    Better commentary

--- Chapter2/bitwiseOPs/2-9.c
+++ Chapter2/bitwiseOPs/2-9.c
@@ -2,7 +2,7 @@
 
 /*
 Exercise 2-9. In a two's complement number system, x &= (x-1) deletes the rightmost 1-bit
-in x. Explain why. Use this observation to write a faster version of bitcount. 
+in x. Explain why. Use this observation to write a faster version of bitcount.  -C Programming Language 2nd Edition 
 */
 
 int bitcount(unsigned x)
@@ -20,20 +20,24 @@ int bitcount(unsigned x)
 
 int main(void)
 {    
-    unsigned x = 6; //00000110 in binary
+    unsigned x = 6; //00000110 in binary (asumming little-endian) 
     x &= (x-1); 
     /*
         This line takes: 
-        00000110(x) and compares the bits with 00000101(x-1) in my example
-        And the result: 00000100 
+        00000110(x) and ANDs the bits with 00000101(x-1) in my example
+        And the result: 00000100 which is literally the highschool bool logic lectures if you remember the de'morgan's law 
+        but for multi-bit binary notation instead of just one 1 and 0   
     */
     int input;
     printf("Gimme number to see 1 bits in it:");
-    fflush(stdout);
+    fflush(stdout); 
+    //stdio is line buffered by default it flushes when sees \n or program ends and return() calls clean up to flush the buffers so
+    //it's better to flush before scanf since there is no '\n' at the end but if you don't want to do it you can change it with _IONBF flag in setvbuf() function   
     scanf("%d",&input);
 
     printf("%d\n",bitcount(input));
     
 
     return(0);
+}
 }
